c2c1b01ee5c4d3304f786817938134e50ee38a14
Lucifer Avada <lucifer.avada@gmail.com>
2017-10-19 18:59:31 -0400

    add aws s3 bucket module

---
+++ bucket/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "AWS Region"
+}
+
+variable "name" {
+  description = "AWS S3 Bucket Name"
+}
+
+variable "acl" {
+  description = "AWS S3 Bucket ACL Policy"
+}
